9c35f50ccd2b5fc2ded118bccffc1f115ebe2fc3
admin <admin@admindeMacBook-Pro-2.local>
2021-04-17 14:06:24 +0800

    temp

---
+++ pages/api/news.js
@@ -0,0 +1,76 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+    res.statusCode = 200;
+    res.json({
+        code: 200,
+        data: [
+            {
+                title:"美国务卿称人不是谈判的筹码华春莹:讲得很对!立即让孟晚舟回国",
+                pic:"http://p3.pstatp.com/list/190x124/tos-cn-i-0004/e1e9ed6f7ba24187bd1f47659ed77b78",
+                kind:"视频",
+                media:"燃新闻视频",
+                footpic:"http://p6-tt-ipv6.byteimg.com/large/dc0b000659d4b3d27841",
+                numofcomment:"27评论",
+                time:"8分钟前"
+
+
+            },
+            {
+                title:"2020年美国侵犯人权报告｜美国疫情严重失控酿成人间悲剧",
+                pic:"http://p1.pstatp.com/list/190x124/pgc-image/SSZfiER2dhZsTI",
+                kind:"国际",
+                media:"新华网客户端",
+                footpic:"http://p26-tt.byteimg.com/large/pgc-image/03f6307ae99745119fa95747f9d9e525",
+                numofcomment:"2319评论",
+                time:"11分钟前"
+
+
+            },
+            {
+                title:"被遗忘25年的国产片，因尺度太大只能在日本上映，揭露真实人性",
+                pic:"http://p1.pstatp.com/list/tos-cn-i-0004/87ecf9a55b314d8a918129bcff11c214",
+                kind:"视频",
+                media:"第七放映厅",
+                footpic:"http://p6-tt-ipv6.byteimg.com/large/dc0b000659d4b3d27841",
+                numofcomment:"25评论",
+                time:"15分钟前"
+
+
+            },
+            {
+                title:"2020年美国侵犯人权报告｜美国疫情严重失控酿成人间悲剧",
+                pic:"http://p1.pstatp.com/list/190x124/pgc-image/SSZfiER2dhZsTI",
+                kind:"国际",
+                media:"新华网客户端",
+                footpic:"http://p26-tt.byteimg.com/large/pgc-image/03f6307ae99745119fa95747f9d9e525",
+                numofcomment:"2319评论",
+                time:"11分钟前"
+
+
+            },
+            {
+                title:"美国务卿称人不是谈判的筹码华春莹:讲得很对!立即让孟晚舟回国",
+                pic:"http://p3.pstatp.com/list/190x124/tos-cn-i-0004/e1e9ed6f7ba24187bd1f47659ed77b78",
+                kind:"视频",
+                media:"燃新闻视频",
+                footpic:"http://p6-tt-ipv6.byteimg.com/large/dc0b000659d4b3d27841",
+                numofcomment:"27评论",
+                time:"8分钟前"
+
+
+            },
+            {
+                title:"2020年美国侵犯人权报告｜美国疫情严重失控酿成人间悲剧",
+                pic:"http://p1.pstatp.com/list/190x124/pgc-image/SSZfiER2dhZsTI",
+                kind:"国际",
+                media:"新华网客户端",
+                footpic:"http://p26-tt.byteimg.com/large/pgc-image/03f6307ae99745119fa95747f9d9e525",
+                numofcomment:"2319评论",
+                time:"11分钟前"
+
+
+            },
+        ],
+    });
+};
